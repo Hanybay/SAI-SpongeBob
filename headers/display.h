cfb7abaea988df5255611db004a72af10343f24b
Taza67 <tazaakil67@gmail.com>
2023-03-23 10:54:08 +0100

    Premier commit : fichiers initiaux

---
+++ headers/display.h
@@ -0,0 +1,15 @@
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+#include "types.h"
+
+// Constantes
+#define DEFAULT_FRUSTUM { .left = -2048, .right = 2048, .top = 2048, .bottom = -2048, .near = 1024, .far = 4096 }
+#define DEFAULT_EYE_POSITION { DEFAULT_DISTANCE, DEFAULT_DISTANCE, DEFAULT_DISTANCE }
+#define DEFAULT_TARGET_POSITION { 0, 0, 0 }
+
+// Prototypes des fonctions
+// Réalise l'affichage
+void display();
+
+#endif
